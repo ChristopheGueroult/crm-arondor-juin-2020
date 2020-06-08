e1a7e0b8f171737f6a4ba718fceadaceab26ed39
christophe gueroult <christophegueroult@gmail.com>
2020-06-08 10:53:44 +0100

    unit test and interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crm-juin-arondor';
+  public title = 'crm-juin-arondor';
 }
